b9a70e4aab5d7eecdbf8f79f39be73116280d326
esther5540 <iesther5540@gmail.com>
2023-04-23 00:10:02 +0300

    first time

---
+++ src/service.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5253/items';
+
+const apiUrl = axios.create();
+
+apiUrl.interceptors.response.use(
+  (response) => response,
+  (error) => {
+   console.log(error)
+    // reject with error if response status is not 403
+    return Promise.reject(error);
+  }
+);
+
+
+
+export default {
+  getTasks: async () => {
+    const result = await apiUrl.get()
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    console.log('addTask', name)
+    const result = await apiUrl.post(``, { Name: name, Iscomplete: false })
+    return result.data;
+  },
+
+  setCompleted: async (id, isComplete) => {
+    console.log('setCompleted', { id, isComplete })
+    const result = await apiUrl.put(`${id}`, { id, isComplete })
+      
+    return result.data;
+  },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask')
+    await apiUrl.delete(`${id}`)
+  }
+};
